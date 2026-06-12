6a241fde7ac63a3bf867f610cfcc34d0fe45e266
mokshamehta06 <mokshamehta71076@gmail.com>
2026-06-12 21:34:28 +0530

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the interviewAI Server API' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+module.exports = app;
